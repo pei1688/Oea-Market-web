c711f9f8626a510ddd80eee78dea5e92a059dd33
Pei Shang <stean8861@gmail.com>
2026-03-22 17:34:58 +0800

    feat: add isAtStockLimit prop to ActionButtons, show stock limit label

--- src/modules/product/components/product-detail/action-buttons.tsx
+++ src/modules/product/components/product-detail/action-buttons.tsx
@@ -2,19 +2,21 @@ import { Button } from "@/components/ui/button";
 
 interface ActionButtonsProps {
   isDisabled: boolean;
+  isAtStockLimit: boolean;
   onAddToCart: () => void;
   onBuyNow: () => void;
+  isAdded: boolean;
 }
 
 export const ActionButtons = ({
   isDisabled,
+  isAtStockLimit,
   onAddToCart,
   onBuyNow,
+  isAdded,
 }: ActionButtonsProps) => {
-
-
   return (
-    <div className="flex-col gap-4 flex">
+    <div className="flex flex-col gap-4">
       {/* <Separator className="bg-primary/20 my-8" /> */}
       <Button
         variant="default"
@@ -25,12 +27,39 @@ export const ActionButtons = ({
         立即購買
       </Button>
       <Button
-        variant="default2"
-        className="w-full bg-fuchsia-200/50 text-fuchsia-800"
         onClick={onAddToCart}
-        disabled={isDisabled}
+        disabled={isDisabled || isAdded}
+        variant={"default2"}
+        className="flex-1 text-fuchsia-800 transition-all duration-300 disabled:opacity-90"
       >
-        加入購物車
+        {isAtStockLimit ? (
+          <>庫存已達上限</>
+        ) : isAdded ? (
+          <>
+            <svg
+              width="24px"
+              height="24px"
+              strokeWidth="1"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="currentColor"
+              className="text-green-700"
+            >
+              <path
+                d="M5 13L9 17L19 7"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={isAdded ? "check-path" : ""}
+              ></path>
+            </svg>
+            已加入
+          </>
+        ) : (
+          <>加入購物車</>
+        )}
       </Button>
     </div>
   );
